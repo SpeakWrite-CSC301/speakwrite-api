456758186611db5ba114e5b8d55807d12e7cd508
moonscape09 <munim.adil@outlook.com>
2025-02-24 04:22:38 -0500

    [DEV 2.15] Populating content on sessions
    The chat POST handler now calls the function
    corresponding to the session PATCH endpoints. This
    attains population of content onto specific
    session objects, based on the `session_id` that
    is also visible in the Chat model.
    
    A new endpoint for GETting specific sessions has
    been added to retrieve sessions matching the
    `session_id` path argument. This will be useful
    in file navigation.

--- app/routers/session.py
+++ app/routers/session.py
@@ -11,6 +11,13 @@ def read_sessions():
         return {"error": "DB SERVICE ERROR: Failed to fetch sessions"}
     return response.json()
 
+@router.get("/sessions/{session_id}", tags=["sessions"])
+def read_session(session_id: int): # Gets session matching the given session_id
+    response = requests.get(f"{DB_SERVICE_URL}/sessions/{session_id}")
+    if response.status_code != 200:
+        return {"error": "DB SERVICE ERROR: Failed to fetch requested session."}
+    return response.json()
+
 @router.post("/sessions", tags=["sessions"])
 def create_session(session: Session):
     response = requests.post(f"{DB_SERVICE_URL}/sessions", json=session.model_dump())
@@ -19,7 +26,7 @@ def create_session(session: Session):
     return response.json()
 
 @router.patch("/sessions/{session_id}", tags=["sessions"])
-def update_session_name(session_id: int, new_session_data: dict):
+def update_session(session_id: int, new_session_data: dict):
     response = requests.patch(f"{DB_SERVICE_URL}/sessions/{session_id}", json=new_session_data)
     if response.status_code != 200:
         return {"error": "DB SERVICE ERROR: Failed to update session."}
