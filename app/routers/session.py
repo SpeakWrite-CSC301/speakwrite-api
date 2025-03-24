1e80583286ab9d92246a3d743b407068edac39d2
ahmedz45 <zuhayr.ahmed@mail.utoronto.ca>
2025-03-24 02:09:02 -0400

    Added jwt to validate users

--- app/routers/session.py
+++ app/routers/session.py
@@ -1,40 +1,58 @@
-from fastapi import APIRouter;
+from fastapi import APIRouter, Depends, Query;
 from schemas import Session, DB_SERVICE_URL
 import requests
+from typing import Optional
+from .user import get_current_user
 
 router = APIRouter()
 
 @router.get("/sessions", tags=["sessions"])
-def read_sessions():
-    response = requests.get(f"{DB_SERVICE_URL}/sessions")
+def read_sessions(current_user: int = Depends(get_current_user)): # Gets all sessions
+    response = None
+    if current_user:
+        response = requests.get(f"{DB_SERVICE_URL}/sessions?user_id={current_user}")
+    else:
+        response = requests.get(f"{DB_SERVICE_URL}/sessions")
     if response.status_code != 200:
         return {"error": "DB SERVICE ERROR: Failed to fetch sessions"}
     return response.json()
 
 @router.get("/sessions/{session_id}", tags=["sessions"])
-def read_session(session_id: int): # Gets session matching the given session_id
-    response = requests.get(f"{DB_SERVICE_URL}/sessions/{session_id}")
+def read_session(session_id: int, current_user: int = Depends(get_current_user)): # Gets session matching the given session_id
+    response = None
+    if current_user:
+        response = requests.get(f"{DB_SERVICE_URL}/sessions/{session_id}?user_id={current_user}")
+    else:
+        response = requests.get(f"{DB_SERVICE_URL}/sessions/{session_id}")
     if response.status_code != 200:
         return {"error": "DB SERVICE ERROR: Failed to fetch requested session."}
     return response.json()
 
 @router.post("/sessions", tags=["sessions"])
-def create_session(session: Session):
-    response = requests.post(f"{DB_SERVICE_URL}/sessions", json=session.model_dump())
+def create_session(session: Session, current_user: int = Depends(get_current_user)):
+    response = requests.post(f"{DB_SERVICE_URL}/sessions?user_id={current_user}", json=session.model_dump())
     if response.status_code != 200:
         return {"error": "DB SERVICE ERROR: Failed to create session."}
     return response.json()
 
 @router.patch("/sessions/{session_id}", tags=["sessions"])
-def update_session(session_id: int, new_session_data: dict):
-    response = requests.patch(f"{DB_SERVICE_URL}/sessions/{session_id}", json=new_session_data)
+def update_session(session_id: int, new_session_data: dict, current_user: int = Depends(get_current_user)):
+    response = None
+    if current_user:
+        response = requests.patch(f"{DB_SERVICE_URL}/sessions/{session_id}?user_id={current_user}", json=new_session_data)
+    else:   
+        response = requests.patch(f"{DB_SERVICE_URL}/sessions/{session_id}", json=new_session_data)
     if response.status_code != 200:
         return {"error": "DB SERVICE ERROR: Failed to update session."}
     return response.json()
 
 @router.delete("/sessions/{session_id}", tags=["sessions"])
-def delete_session(session_id: int):
-    response = requests.delete(f"{DB_SERVICE_URL}/sessions/{session_id}")
+def delete_session(session_id: int, current_user: int = Depends(get_current_user)):
+    response = None
+    if current_user:
+        response = requests.delete(f"{DB_SERVICE_URL}/sessions/{session_id}?user_id={current_user}")
+    else:
+        response = requests.delete(f"{DB_SERVICE_URL}/sessions/{session_id}")
     if response.status_code != 200:
        return {"error": "DB SERVICE ERROR: Failed to delete session."}
     return response.json()
